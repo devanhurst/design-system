419ee698582d85bf5416cf879d278bfb4ab01f1e
Marianne Borowiak <marianneb@nulogy.com>
2020-07-08 17:29:49 -0400

    fix eslint configurations to cover ts files

--- components/.storybook/webpack.config.js
+++ components/.storybook/webpack.config.js
@@ -1,20 +1,21 @@
+/* eslint-disable */
 const path = require("path");
 module.exports = {
   module: {
     rules: [
       {
         test: /\.story\.js?$/,
-        loaders: [require.resolve('@storybook/addon-storysource/loader')],
-        enforce: 'pre',
+        loaders: [require.resolve("@storybook/addon-storysource/loader")],
+        enforce: "pre"
       },
       {
         test: /\.svg$/,
-        loader: 'svg-sprite-loader'
+        loader: "svg-sprite-loader"
       },
       {
         test: /\.css$/,
         loaders: ["style-loader", "css-loader"]
       }
-    ],
-  },
+    ]
+  }
 };
